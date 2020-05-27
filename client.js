485a6f408e962bfc280a6d0eb06bb458693abd11
Sahil <sahilkkrazy@gmail.com>
2020-05-27 22:47:16 +0530

    add search query

--- client.js
+++ client.js
@@ -3,36 +3,40 @@ var client = ldap.createClient({
     url: 'ldap://ldap.forumsys.com:389'
 });
 
-client.bind('cn=read-only-admin,dc=example,dc=com', 'password', function(err) {
+var base = 'cn=read-only-admin,dc=example,dc=com';
+var password = 'password';
+var username = "testuser22";
+
+client.bind(base, password, function(err) {
     if(err){
         console.log('Error occurred while binding', err);
     }else {
         console.log("success");
 
-        // var base = 'cn=admin,dc=example,dc=com';
-        // var search_options = {
-        //     scope: 'sub',
-        //     filter: '(&(objectClass=*)(CN=' + username + '))',
-        //     attrs: 'memberOf'
-        // };
-        // client.search(base, search_options, function (err, res) {
-        //     if (err) {
-        //         console.log('Error occurred while ldap search');
-        //     } else {
-        //         res.on('searchEntry', function (entry) {
-        //             console.log('Entry', JSON.stringify(entry.object));
-        //         });
-        //         res.on('searchReference', function (referral) {
-        //             console.log('Referral', referral);
-        //         });
-        //         res.on('error', function (err) {
-        //             console.log('Error is', err);
-        //         });
-        //         res.on('end', function (result) {
-        //             console.log('Result is', result);
-        //         });
-        //     }
-        // });
+
+        var search_options = {
+            scope: 'sub',
+            filter: '(&(objectClass=*)(CN=' + username + '))',
+            attributes: ['dn', 'sn', 'cn']
+        };
+        client.search(base, search_options, function (err, res) {
+            if (err) {
+                console.log('Error occurred while ldap search');
+            } else {
+                res.on('searchEntry', function (entry) {
+                    console.log('Entry', JSON.stringify(entry.object));
+                });
+                res.on('searchReference', function (referral) {
+                    console.log('Referral', referral);
+                });
+                res.on('error', function (err) {
+                    console.log('Error is', err);
+                });
+                res.on('end', function (result) {
+                    console.log('Result is', result);
+                });
+            }
+        });
 
 
     }
